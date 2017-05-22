916932d9fe5b93c156893eaa873bf14e7bd4b499
nucknine <nucknine@gmail.com>
2017-05-22 17:55:48 +0300

    go

--- demo1/js/common.js
+++ demo1/js/common.js
@@ -33,6 +33,7 @@ $(document).ready(function(){
 //burger-menu
 //---------------------------------------------------------
 var overlay = $('.page-overlay');
+
 	$('.burger-icon').click(function(){
 		$('.burger-icon__line_line1').toggleClass('burger-icon__line_line-open1');
 		$('.burger-icon__line_line2').toggleClass('burger-icon__line_line-open2');
@@ -40,17 +41,17 @@ var overlay = $('.page-overlay');
 		$('.burger-menu').toggleClass('burger-menu_visible');
 		$('.page-overlay').toggleClass('page-overlay_visible');
 		$('body').toggleClass('overflow');
-
 		if(overlay.hasClass('page-overlay_visible')) {
 			console.log('overlay is visible');
 			$(document).bind('touchmove', false);
-		} else {
-			$(document).bind('touchmove', true);
 		}
 	});
 
 	$('.page-overlay').click(function(){
 		$('.burger-icon__line_line1, .burger-icon__line_line2, .burger-icon__line_line3, .burger-menu, .page-overlay, body, .burger-icon, .video').removeClass('burger-icon__line_line-open1 burger-icon__line_line-open2 burger-icon__line_line-open3 burger-menu_visible page-overlay_visible overflow burger-icon_hide video_visible');
+		console.log('overlay is not visible');
+		$(document).bind('touchmove', false);
+
 	});
 
 	$('#js-watch-video-head, #js-watch-video-content').click(function(){
